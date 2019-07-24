7098177d54f22fce8c0a1f20437d5ccdd70bb6ff
Jorge Tapia <jorge@profapps.co>
2019-07-24 18:34:46 -0500

    Pin app bar
    - Add dummy list of items

--- lib/screens/tabs/home/home_tab.dart
+++ lib/screens/tabs/home/home_tab.dart
@@ -12,7 +12,20 @@ class _HomeTabState extends State<HomeTab> {
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
+          pinned: true,
           title: const Text('Welcome to SWAT Nation'),
+        ),
+        SliverFixedExtentList(
+          itemExtent: 50.0,
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.lightBlue[100 * (index % 9)],
+                child: Text('list item $index'),
+              );
+            },
+          ),
         ),
       ],
     );
