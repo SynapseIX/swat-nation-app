92d7d191c9be4cd0cd8a5e50ffd250a539f6a9b9
Jorge Tapia <jorge@profapps.co>
2019-07-24 19:28:16 -0500

    Add sliver text header stub

---
+++ lib/widgets/slivers/headers/sliver_text_header.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+/// Creates a text sliver header.
+class SliverTextHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
