8e229fa29042bdd024b847ab9144c5ae309cbadd
Jorge Tapia <jorge@profapps.co>
2019-07-24 19:23:29 -0500

    Create widget for image cards

--- lib/screens/tabs/home/home_tab.dart
+++ lib/screens/tabs/home/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swat_nation/widgets/cards/image_card.dart';
 
 /// Represents the home tab screen.
 class HomeTab extends StatefulWidget {
@@ -15,17 +16,14 @@ class _HomeTabState extends State<HomeTab> {
           pinned: true,
           title: const Text('Welcome to SWAT Nation'),
         ),
-        SliverFixedExtentList(
-          itemExtent: 50.0,
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.lightBlue[100 * (index % 9)],
-                child: Text('list item $index'),
-              );
-            },
-          ),
+        SliverList(
+          delegate: SliverChildListDelegate(<Widget>[
+            const ImageCard(
+              aspectRatio: 16.0 / 9.0,
+              src: 'https://firebasestorage.googleapis.com/v0/b/swat-nation.appspot.com/o/tourney-posters%2F%2317%20Once%20upon%20a%20SWAT.png?alt=media&token=1fd124d1-6ea8-4a1d-80e5-1b8274b230cb',
+              margin: EdgeInsets.all(8.0),
+            ),
+          ]),
         ),
       ],
     );
---
+++ lib/widgets/cards/image_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+/// Creates a card that holds a single image fetched remotely.
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    @required this.src,
+    @required this.aspectRatio,
+    Key key,
+    this.margin = EdgeInsets.zero,
+    this.padding = EdgeInsets.zero,
+    this.width = double.infinity,
+    this.height = double.infinity,
+  }) : super(key: key);
+
+  final String src;
+  final double aspectRatio;
+  final EdgeInsets margin;
+  final EdgeInsets padding;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: aspectRatio,
+      child: Container(
+        width: width,
+        height: height,
+        margin: margin,
+        padding: padding,
+        child: Card(
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: src,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
