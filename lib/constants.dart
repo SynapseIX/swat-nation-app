175374adc1631e082062cde3dbd7277cd38abb5c
Jorge Tapia <jorge@profapps.co>
2019-07-29 17:50:44 -0500

    Implement image cache
    - Add cached_network_image package

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
+// SWAT Nation logo
+const String kLogo = 'https://static1.squarespace.com/static/5bfb2111372b964077959077/t/5bfcbd661ae6cf259c75a2ad/1563085290045/?format=500w';
+
 // SWAT Nation social accounts
 const String kWebsite = 'https://swatnation.net';
 const String kFacebookGroup = 'https://facebook.com/groups/swat-nation';
