e6c32b4871be3dba39740970a4bdaf580294f005
Jorge Tapia <jorge@profapps.co>
2019-08-01 18:24:00 -0500

    Add confirm password functionality

--- lib/blocs/sign_in_screen_bloc.dart
+++ lib/blocs/sign_in_screen_bloc.dart
@@ -5,21 +5,47 @@ import 'package:swat_nation/mixins/email_password_validator.dart';
 class SignInScreenBloc extends BaseBloc with EmailPasswordValidator {
   final BehaviorSubject<String> _emailSubject = BehaviorSubject<String>();
   final BehaviorSubject<String> _passwordSubject = BehaviorSubject<String>();
+  final BehaviorSubject<String> _confirmPasswordSubject = BehaviorSubject<String>();
 
-  Stream<String> get emailStream => _emailSubject.stream.transform(validateEmail);
-  Stream<String> get passwordStream => _passwordSubject.stream.transform(validatePassword);
-  Stream<bool> get submitValidStream => Observable
-    .combineLatest2(emailStream, passwordStream, (String e, String p) => true);
+  Stream<String> get emailStream => _emailSubject
+    .stream
+    .transform(validateEmail);
+  
+  Stream<String> get passwordStream => _passwordSubject
+    .stream
+    .transform(validatePassword);
+
+  Stream<String> get confirmPasswordStream => _confirmPasswordSubject
+    .stream
+    .transform(validatePassword);
+  
+  Stream<bool> get signInValidStream => Observable
+    .combineLatest2(
+      emailStream,
+      passwordStream,
+      (String e, String p) => true,
+    );
+
+  Stream<bool> get signUpValidStream => Observable
+    .combineLatest3(
+      emailStream,
+      passwordStream,
+      confirmPasswordStream,
+      (String e, String p, String cp) => 0 == p.compareTo(cp),
+    );
 
   String get emailValue => _emailSubject.value;
-  String get passwordValue => _passwordSubject.value; 
+  String get passwordValue => _passwordSubject.value;
+  String get confirmPasswordValue => _confirmPasswordSubject.value;
 
   void Function(String) get onChangeEmail => _emailSubject.sink.add;
   void Function(String) get onChangePassword => _passwordSubject.sink.add;
+  void Function(String) get onChangeConfirmPassword => _passwordSubject.sink.add;
   
   @override
   void dispose() {
     _emailSubject.close();
     _passwordSubject.close();
+    _confirmPasswordSubject.close();
   }
 }
