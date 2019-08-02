3ac6f47d17d76a2b9ddf18d2c3f371df79d6dcfe
Jorge Tapia <jorge@profapps.co>
2019-08-02 10:20:46 -0500

    [iOS] Setup Facebook SDK

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation AppDelegate
 
@@ -7,8 +8,27 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [FIRApp configure];
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  
   // Override point for customization after application launch.
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+    didFinishLaunchingWithOptions:launchOptions];
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+
+- (BOOL)application:(UIApplication *)application 
+            openURL:(NSURL *)url 
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+    openURL:url
+    sourceApplication:sourceApplication
+    annotation:annotation
+  ];
+  // Add any custom logic here.
+  return handled;
+} 
 
 @end
