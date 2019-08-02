e92784404f54c3b6636536c8396b9dd7b74b44fe
Jorge Tapia <jorge@profapps.co>
2019-08-01 19:07:03 -0500

    Remove Card widget

--- lib/screens/auth/create_account_screen.dart
+++ lib/screens/auth/create_account_screen.dart
@@ -43,126 +43,121 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
         child: SafeArea(
           child: Center(
             child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Card(
-                  child: Container(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        // Logo
-                        Container(
-                          margin: const EdgeInsets.only(top: 16.0),
-                          decoration: const BoxDecoration(
-                            color: Color(0xFF333333),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Hero(
-                            tag: 'swat_nation_logo',
-                            child: CachedNetworkImage(
-                              imageUrl: kLogo,
-                              fadeInDuration: Duration(milliseconds: 300),
-                              width: 120.0,
-                              height: 120.0,
-                            ),
-                          ),
+              child: Container(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    // Logo
+                    Container(
+                      margin: const EdgeInsets.only(top: 16.0),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF333333),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Hero(
+                        tag: 'swat_nation_logo',
+                        child: CachedNetworkImage(
+                          imageUrl: kLogo,
+                          fadeInDuration: Duration(milliseconds: 300),
+                          width: 120.0,
+                          height: 120.0,
                         ),
-
-                        const SizedBox(height: 8.0),
-
-                        // Email field
-                        StreamBuilder<String>(
-                          stream: bloc.emailStream,
-                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                            return TextField(
-                              autocorrect: false,
-                              keyboardType: TextInputType.emailAddress,
-                              textInputAction: TextInputAction.next,
-                              focusNode: emailNode,
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                hintText: 'user@example.com',
-                                errorText: snapshot.error,
-                              ),
-                              onSubmitted: (String text) {
-                                emailNode.nextFocus();
-                              },
-                              onChanged: bloc.onChangeEmail,
-                            );
-                          },
-                        ),
-
-                        // Password field
-                        StreamBuilder<String>(
-                          stream: bloc.passwordStream,
-                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                            return TextField(
-                              autocorrect: false,
-                              obscureText: true,
-                              textInputAction: TextInputAction.next,
-                              focusNode: passwordNode,
-                              decoration: InputDecoration(
-                                labelText: 'Password',
-                                hintText: 'password',
-                                errorText: snapshot.error,
-                              ),
-                              onSubmitted: (String text) {
-                                passwordNode.nextFocus();
-                              },
-                              onChanged: bloc.onChangePassword,
-                            );
-                          },
-                        ),
-
-                        // Confirm password field
-                        StreamBuilder<String>(
-                          stream: bloc.confirmPasswordStream,
-                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                            return TextField(
-                              autocorrect: false,
-                              obscureText: true,
-                              textInputAction: TextInputAction.go,
-                              focusNode: confirmPasswordNode,
-                              decoration: InputDecoration(
-                                labelText: 'Confirm Password',
-                                hintText: 'confirm password',
-                                errorText: snapshot.error,
-                              ),
-                              onSubmitted: (String text) {
-                                confirmPasswordNode.unfocus();
-                              },
-                              onChanged: bloc.onChangeConfirmPassword,
-                            );
-                          },
-                        ),
-
-                        const SizedBox(height: 24.0),
-
-                        // Create Account button
-                        Container(
-                          width: double.infinity,
-                          height: 40.0,
-                          child: StreamBuilder<bool>(
-                            stream: bloc.createAccountValidStream,
-                            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                              return RaisedButton(
-                                child: const Text('Create Account'),
-                                onPressed: snapshot.hasData
-                                  ? () {
-                                    print('TODO: Register user on Firebase');
-                                    print('Email: ${bloc.emailValue}');
-                                    print('Password: ${bloc.passwordValue}');
-                                    _dismissKeyboard();
-                                  }
-                                  : null,
-                              );
-                            },
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+
+                    const SizedBox(height: 8.0),
+
+                    // Email field
+                    StreamBuilder<String>(
+                      stream: bloc.emailStream,
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return TextField(
+                          autocorrect: false,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          focusNode: emailNode,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'user@example.com',
+                            errorText: snapshot.error,
+                          ),
+                          onSubmitted: (String text) {
+                            emailNode.nextFocus();
+                          },
+                          onChanged: bloc.onChangeEmail,
+                        );
+                      },
+                    ),
+
+                    // Password field
+                    StreamBuilder<String>(
+                      stream: bloc.passwordStream,
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return TextField(
+                          autocorrect: false,
+                          obscureText: true,
+                          textInputAction: TextInputAction.next,
+                          focusNode: passwordNode,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'password',
+                            errorText: snapshot.error,
+                          ),
+                          onSubmitted: (String text) {
+                            passwordNode.nextFocus();
+                          },
+                          onChanged: bloc.onChangePassword,
+                        );
+                      },
+                    ),
+
+                    // Confirm password field
+                    StreamBuilder<String>(
+                      stream: bloc.confirmPasswordStream,
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return TextField(
+                          autocorrect: false,
+                          obscureText: true,
+                          textInputAction: TextInputAction.go,
+                          focusNode: confirmPasswordNode,
+                          decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            hintText: 'confirm password',
+                            errorText: snapshot.error,
+                          ),
+                          onSubmitted: (String text) {
+                            confirmPasswordNode.unfocus();
+                          },
+                          onChanged: bloc.onChangeConfirmPassword,
+                        );
+                      },
+                    ),
+
+                    const SizedBox(height: 24.0),
+
+                    // Create Account button
+                    Container(
+                      width: double.infinity,
+                      height: 40.0,
+                      child: StreamBuilder<bool>(
+                        stream: bloc.createAccountValidStream,
+                        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                          return RaisedButton(
+                            child: const Text('Create Account'),
+                            onPressed: snapshot.hasData
+                              ? () {
+                                print('TODO: Register user on Firebase');
+                                print('Email: ${bloc.emailValue}');
+                                print('Password: ${bloc.passwordValue}');
+                                _dismissKeyboard();
+                              }
+                              : null,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
--- lib/screens/auth/sign_in_screen.dart
+++ lib/screens/auth/sign_in_screen.dart
@@ -42,145 +42,139 @@ class _SignInScreenState extends State<SignInScreen> {
         child: SafeArea(
           child: Center(
             child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Card(
-                  child: Container(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        // Logo
-                        Container(
-                          margin: const EdgeInsets.only(top: 16.0),
-                          decoration: const BoxDecoration(
-                            color: Color(0xFF333333),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Hero(
-                            tag: 'swat_nation_logo',
-                            child: CachedNetworkImage(
-                              imageUrl: kLogo,
-                              fadeInDuration: Duration(milliseconds: 300),
-                              width: 120.0,
-                              height: 120.0,
-                            ),
-                          ),
+              child: Container(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    // Logo
+                    Container(
+                      margin: const EdgeInsets.only(top: 16.0),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF333333),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Hero(
+                        tag: 'swat_nation_logo',
+                        child: CachedNetworkImage(
+                          imageUrl: kLogo,
+                          fadeInDuration: Duration(milliseconds: 300),
+                          width: 120.0,
+                          height: 120.0,
                         ),
+                      ),
+                    ),
 
-                        const SizedBox(height: 8.0),
+                    const SizedBox(height: 8.0),
 
-                        // Email field
-                        StreamBuilder<String>(
-                          stream: bloc.emailStream,
-                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                            return TextField(
-                              autocorrect: false,
-                              keyboardType: TextInputType.emailAddress,
-                              textInputAction: TextInputAction.next,
-                              focusNode: emailNode,
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                hintText: 'user@example.com',
-                                errorText: snapshot.error,
-                              ),
-                              onSubmitted: (String text) {
-                                emailNode.nextFocus();
-                              },
-                              onChanged: bloc.onChangeEmail,
-                            );
+                    // Email field
+                    StreamBuilder<String>(
+                      stream: bloc.emailStream,
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return TextField(
+                          autocorrect: false,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          focusNode: emailNode,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'user@example.com',
+                            errorText: snapshot.error,
+                          ),
+                          onSubmitted: (String text) {
+                            emailNode.nextFocus();
                           },
-                        ),
+                          onChanged: bloc.onChangeEmail,
+                        );
+                      },
+                    ),
 
-                        // Password field
-                        StreamBuilder<String>(
-                          stream: bloc.passwordStream,
-                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                            return TextField(
-                              autocorrect: false,
-                              obscureText: true,
-                              textInputAction: TextInputAction.go,
-                              focusNode: passwordNode,
-                              decoration: InputDecoration(
-                                labelText: 'Password',
-                                hintText: 'password',
-                                errorText: snapshot.error,
-                              ),
-                              onSubmitted: (String text) {
-                                passwordNode.unfocus();
-                              },
-                              onChanged: bloc.onChangePassword,
-                            );
-                          },
-                        ),
-
-                        const SizedBox(height: 24.0),
-
-                        // Sign In button
-                        Container(
-                          width: double.infinity,
-                          height: 40.0,
-                          child: StreamBuilder<bool>(
-                            stream: bloc.signInValidStream,
-                            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                              return RaisedButton(
-                                child: const Text('Sign In'),
-                                onPressed: snapshot.hasData
-                                  ? () {
-                                    print('TODO: Sign in to Firebase');
-                                    print('Email: ${bloc.emailValue}');
-                                    print('Password: ${bloc.passwordValue}');
-                                    _dismissKeyboard();
-                                  }
-                                  : null,
-                              );
-                            },
+                    // Password field
+                    StreamBuilder<String>(
+                      stream: bloc.passwordStream,
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return TextField(
+                          autocorrect: false,
+                          obscureText: true,
+                          textInputAction: TextInputAction.go,
+                          focusNode: passwordNode,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'password',
+                            errorText: snapshot.error,
                           ),
-                        ),
+                          onSubmitted: (String text) {
+                            passwordNode.unfocus();
+                          },
+                          onChanged: bloc.onChangePassword,
+                        );
+                      },
+                    ),
 
-                        const SizedBox(height: 16.0),
+                    const SizedBox(height: 24.0),
 
-                        // Create Account / Forgot Password
-                        Row(
-                          children: <Widget>[
-                            GestureDetector(
-                              onTap: () {
+                    // Sign In button
+                    Container(
+                      width: double.infinity,
+                      height: 40.0,
+                      child: StreamBuilder<bool>(
+                        stream: bloc.signInValidStream,
+                        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                          return RaisedButton(
+                            child: const Text('Sign In'),
+                            onPressed: snapshot.hasData
+                              ? () {
+                                print('TODO: Sign in to Firebase');
+                                print('Email: ${bloc.emailValue}');
+                                print('Password: ${bloc.passwordValue}');
                                 _dismissKeyboard();
-                                Navigator.of(context).push(
-                                  MaterialPageRoute<CreateAccountScreen>(
-                                    builder: (BuildContext context)=> CreateAccountScreen(),
-                                  ),
-                                );
-                              },
-                              child: const Text(
-                                'Create Account',
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                            Spacer(),
-                            GestureDetector(
-                              onTap: () => print('TODO: implement'),
-                              child: const Text(
-                                'Forgot Password?',
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ],
-                        ),
+                              }
+                              : null,
+                          );
+                        },
+                      ),
+                    ),
 
-                        const SizedBox(height: 24.0),
+                    const SizedBox(height: 16.0),
 
-                        // Login with Facebook button
-                        FlatButton(
-                          child: Image.asset('assets/images/continue_with_facebook.png'),
-                          onPressed: () {
-                            print('TODO: implement');
+                    // Create Account / Forgot Password
+                    Row(
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
                             _dismissKeyboard();
+                            Navigator.of(context).push(
+                              MaterialPageRoute<CreateAccountScreen>(
+                                builder: (BuildContext context)=> CreateAccountScreen(),
+                              ),
+                            );
                           },
+                          child: const Text(
+                            'Create Account',
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () => print('TODO: implement'),
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
                         ),
                       ],
                     ),
-                  ),
+
+                    const SizedBox(height: 24.0),
+
+                    // Login with Facebook button
+                    FlatButton(
+                      child: Image.asset('assets/images/continue_with_facebook.png'),
+                      onPressed: () {
+                        print('TODO: implement');
+                        _dismissKeyboard();
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
