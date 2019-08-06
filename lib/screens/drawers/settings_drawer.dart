60660aba92778806b3e44e095f0c436e7bc5458a
Jorge Tapia <jorge@profapps.co>
2019-08-06 16:14:21 -0500

    Cache profile header image

--- lib/screens/drawers/settings_drawer.dart
+++ lib/screens/drawers/settings_drawer.dart
@@ -252,39 +252,43 @@ class _AuthHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return UserAccountsDrawerHeader(
-      accountName: Text(user.displayName),
-      accountEmail: Text(user.email),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(kDefaultProfileHeader),
+    return Stack(
+      children: <Widget>[
+        CachedNetworkImage(
+          imageUrl: kDefaultProfileHeader,
           fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 300),
         ),
-      ),
-      currentAccountPicture: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xFF333333),
-          shape: BoxShape.circle,
-          border: Border.all(
-            width: 2.0,
-            color: Colors.white,
+        UserAccountsDrawerHeader(
+          accountName: Text(user.displayName),
+          accountEmail: Text(user.email),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
           ),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(40.0),
-          child: CachedNetworkImage(
-            imageUrl: user.photoUrl,
-            width: 40.0,
-            height: 40.0,
-            fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+          currentAccountPicture: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFF333333),
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 2.0,
+                color: Colors.white,
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40.0),
+              child: CachedNetworkImage(
+                imageUrl: user.photoUrl,
+                fit: BoxFit.cover,
+                fadeInDuration: Duration(milliseconds: 300),
+              ),
+            ),
           ),
+          onDetailsPressed: () {
+            Navigator.of(context).pop();
+            print('TODO: navigate to profile screen');
+          },
         ),
-      ),
-      onDetailsPressed: () {
-        Navigator.of(context).pop();
-        print('TODO: navigate to profile screen');
-      },
+      ],
     );
   }
 }
