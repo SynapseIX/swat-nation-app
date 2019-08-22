74ee6518cd11561f01f41f266fe96b6bac74420d
Jorge Tapia <jorge@profapps.co>
2019-08-22 11:50:23 -0500

    Add create clip screen stub

---
+++ lib/screens/clips/create_clip_scren.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Represent the creat clip screen.
+class CreateClipScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CreateClipScreenState();
+  }
+}
+
+class _CreateClipScreenState extends State<CreateClipScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
