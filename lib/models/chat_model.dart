0020feb82b89faae87a3ae8647d31d1714d20ce8
Jorge Tapia <jorge@profapps.co>
2019-08-23 13:35:08 -0500

    Create chat model class

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:swat_nation/base/base_model.dart';
+
+/// Represents a chat message instance.
+class ChatModel extends BaseModel {
+  ChatModel({
+    @required this.message,
+    @required this.createdAt,
+    @required this.author,
+    @required this.displayName,
+    this.photoUrl,
+  });
+
+  ChatModel.fromSnapshot(DocumentSnapshot snapshot) {
+    message = snapshot.data['message'];
+    createdAt = snapshot.data['createdAt'];
+    author = snapshot.data['author'];
+    displayName = snapshot.data['displayName'];
+    photoUrl = snapshot.data['photoUrl'];
+  }
+
+  ChatModel.blank();
+  
+  String message;
+  Timestamp createdAt;
+  String author;
+  String displayName;
+  String photoUrl;
+
+  @override
+  String toString() {
+    return '''
+    message: $message
+    createdAt: $createdAt
+    author: $author
+    displayName: $displayName
+    photoUrl: $photoUrl
+    ''';
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'message': message,
+      'createdAt': createdAt,
+      'author': author,
+      'displayName': displayName,
+      'photoUrl': photoUrl,
+    };
+  }
+}
