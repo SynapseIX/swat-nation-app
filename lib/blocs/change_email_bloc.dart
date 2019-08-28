c95a7cd2d5a49c1daf0f60d51f24aa54e90eb696
Jorge Tapia <jorge@profapps.co>
2019-08-28 14:21:19 -0500

    Create BLoC stub

---
+++ lib/blocs/change_email_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:swat_nation/base/base_bloc.dart';
+
+/// BLoC containing logic to manage the change email screen.
+class ChangeEmailBloc extends BaseBloc {
+  @override
+  void dispose() {
+    print('ChangeEmailBloc disposed');
+  }
+}
