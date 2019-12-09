79d54fc66ff18300b106b08af6a889ce2140b131
Jorge Tapia <jorge@profapps.co>
2019-12-09 15:07:21 -0500

    Make constant fields private

--- lib/blocs/about_bloc.dart
+++ lib/blocs/about_bloc.dart
@@ -3,15 +3,16 @@ import 'package:swat_nation/base/base_bloc.dart';
 import 'package:swat_nation/mixins/about_transfromer.dart';
 import 'package:swat_nation/models/about_model.dart';
 
-class AboutBloc extends BaseBloc with AboutTransformer {
-  final String collection = 'about';
-  final String path = 'eKW6bxm4WwZunbeOFm4g';
+const String _collection = 'about';
+const String _path = 'eKW6bxm4WwZunbeOFm4g';
 
+/// BLoC that contains logic to extract data for the About Us screen.
+class AboutBloc extends BaseBloc with AboutTransformer {
   final Firestore _firestore = Firestore.instance;
 
   Stream<AboutModel> get infoStream => _firestore
-    .collection(collection)
-    .document(path)
+    .collection(_collection)
+    .document(_path)
     .snapshots()
     .transform(transformAbout);
 
